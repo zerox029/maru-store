cbae7720514530878797c5dd0d56fac0ccdff1aa
Étienne Plante <zerox029@gmail.com>
2020-11-27 22:10:54 -0500

    Made components out of directory, menu-item and homepage

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import './directory.styles.scss';
+import { sections } from './directory.data';
+import MenuItem from '../menu-item/menu-item.component';
+
+class Directory extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: sections
+    }
+  }
+
+  render() {
+    return (
+      <div className='directory-menu'>
+        {
+          this.state.sections.map(({title, imageUrl, id, size}) => (
+            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+          ))
+        }
+      </div>
+    );
+  }
+}
+
+export default Directory;
---
+++ src/components/directory/directory.data.js
@@ -0,0 +1,34 @@
+export const sections = [
+  {
+    title: 'figures',
+    imageUrl: 'https://i.ibb.co/s1Mqbkw/fc303a0eb1a58c81325d946db69e1ece.jpg',
+    id: 1,
+    linkUrl: 'shop/figures'
+  },
+  {
+    title: 'plushies',
+    imageUrl: 'https://i.ibb.co/YtTGyKP/2020-11-27-215159.png',
+    id: 2,
+    linkUrl: 'shop/plushies'
+  },
+  {
+    title: 'apparel',
+    imageUrl: 'https://i.ibb.co/Xxz1HxC/apparel.jpg',
+    id: 3,
+    linkUrl: 'shop/apparel'
+  },
+  {
+    title: 'goodies',
+    imageUrl: 'https://i.imgur.com/WhvxBV4.png',
+    size: 'large',
+    id: 4,
+    linkUrl: 'shop/womens'
+  },
+  {
+    title: 'music',
+    imageUrl: 'https://i.imgur.com/WBRY3H0.png',
+    size: 'large',
+    id: 5,
+    linkUrl: 'shop/mens'
+  }
+];
