d6dcf780614cda43a673969a63afe460e73067ce
Étienne Plante <zerox029@gmail.com>
2020-11-27 22:22:56 -0500

    :lipstick: Added styling for homepage

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -18,7 +18,7 @@ class Directory extends Component {
       <div className='directory-menu'>
         {
           this.state.sections.map(({title, imageUrl, id, size}) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+            <MenuItem key={id} title={title.toUpperCase()} imageUrl={imageUrl} size={size} />
           ))
         }
       </div>
