4256e217326099d02d332b1c43a025c008ad677e
Étienne Plante <zerox029@gmail.com>
2020-11-28 12:53:49 -0500

    :sparkles: Basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
+import './App.css';
 import HomePage  from './pages/homepage/homepage.component';
+
+const FiguresPage = () => (
+  <div>
+    <h1>Figures</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/figures' component={FiguresPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.data.js
+++ src/components/directory/directory.data.js
@@ -3,32 +3,32 @@ export const sections = [
     title: 'figures',
     imageUrl: 'https://i.ibb.co/s1Mqbkw/fc303a0eb1a58c81325d946db69e1ece.jpg',
     id: 1,
-    linkUrl: 'shop/figures'
+    linkUrl: 'figures'
   },
   {
     title: 'plushies',
     imageUrl: 'https://i.ibb.co/YtTGyKP/2020-11-27-215159.png',
     id: 2,
-    linkUrl: 'shop/plushies'
+    linkUrl: 'plushies'
   },
   {
     title: 'apparel',
     imageUrl: 'https://i.ibb.co/Xxz1HxC/apparel.jpg',
     id: 3,
-    linkUrl: 'shop/apparel'
+    linkUrl: 'apparel'
   },
   {
     title: 'goodies',
     imageUrl: 'https://i.imgur.com/WhvxBV4.png',
     size: 'large',
     id: 4,
-    linkUrl: 'shop/womens'
+    linkUrl: 'womens'
   },
   {
     title: 'music',
     imageUrl: 'https://i.imgur.com/WBRY3H0.png',
     size: 'large',
     id: 5,
-    linkUrl: 'shop/mens'
+    linkUrl: 'mens'
   }
 ];
