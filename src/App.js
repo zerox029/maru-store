d9275e0c9434720cd80907e305c5a07f125ad812
Étienne Plante <zerox029@gmail.com>
2020-11-28 21:53:56 -0500

    :sparkles: Added shop page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import HomePage  from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 
 const FiguresPage = () => (
   <div>
@@ -15,7 +16,8 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/figures' component={FiguresPage} />
+        <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/shop/figures' component={FiguresPage} />
       </Switch>
     </div>
   );
--- src/components/directory/directory.data.js
+++ src/components/directory/directory.data.js
@@ -3,32 +3,32 @@ export const sections = [
     title: 'figures',
     imageUrl: 'https://i.ibb.co/s1Mqbkw/fc303a0eb1a58c81325d946db69e1ece.jpg',
     id: 1,
-    linkUrl: 'figures'
+    linkUrl: 'shop/figures'
   },
   {
     title: 'plushies',
     imageUrl: 'https://i.ibb.co/YtTGyKP/2020-11-27-215159.png',
     id: 2,
-    linkUrl: 'plushies'
+    linkUrl: 'shop/plushies'
   },
   {
     title: 'apparel',
     imageUrl: 'https://i.ibb.co/Xxz1HxC/apparel.jpg',
     id: 3,
-    linkUrl: 'apparel'
+    linkUrl: 'shop/apparel'
   },
   {
     title: 'goodies',
     imageUrl: 'https://i.imgur.com/WhvxBV4.png',
     size: 'large',
     id: 4,
-    linkUrl: 'womens'
+    linkUrl: 'shop/goodies'
   },
   {
     title: 'music',
     imageUrl: 'https://i.imgur.com/WBRY3H0.png',
     size: 'large',
     id: 5,
-    linkUrl: 'mens'
+    linkUrl: 'shop/music'
   }
 ];
