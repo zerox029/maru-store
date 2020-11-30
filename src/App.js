d4953e71685eb6e1dbece129ecebc897ebe97adc
Étienne Plante <zerox029@gmail.com>
2020-11-30 10:42:19 -0500

    :sparkles: Added signin component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage  from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInOutPage from './pages/sign-in-out/sign-in-out.component';
 import Header from './components/header/header.component';
 
 const FiguresPage = () => (
@@ -20,6 +21,7 @@ function App() {
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
         <Route exact path='/shop/figures' component={FiguresPage} />
+        <Route exact path='/signin' component={SignInOutPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-out/sign-in-out.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './sign-in-out.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInOutPage = () => (
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+  </div>
+);
+
+export default SignInOutPage;
