43ca563b1428743c1bc461405af27791fdf83c86
Étienne Plante <zerox029@gmail.com>
2020-11-30 10:00:22 -0500

    :sparkles: Added header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import HomePage  from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 const FiguresPage = () => (
   <div>
@@ -14,6 +15,7 @@ const FiguresPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
