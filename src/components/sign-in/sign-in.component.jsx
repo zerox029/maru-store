1c61eb49fe301124a8058cfddcc1fb19a271de8b
Étienne Plante <zerox029@gmail.com>
2020-12-02 11:12:42 -0500

    Set up redux

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -46,7 +46,7 @@ class SignIn extends Component {
 
           <div className="buttons">
             <CustomButton type="submit"> Sign in </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in with google </CustomButton>
+            <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn> Sign in with google </CustomButton>
           </div>
         </form>
       </div>
